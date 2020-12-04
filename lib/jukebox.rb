5ef7b9123816c04bb8a215e816fcf296e92588f7
John Wisneski <john.j.wisneski@gmail.com>
2020-12-04 23:35:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,9 +17,10 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
-  if user_input.to_i >0 && user_input.to_i <= songs.length
+  track_number = user_input.to_i
+  if track_number >0 && track_number <= songs.length
     binding.pry
-    puts "Playing #{songs[user_input.to_i -1]}"
+    puts "Playing #{songs[track_number -1]}"
   elsif songs.include?(user_input)
     puts "Playing #{songs.find{|song| song == user_input}}"
   else puts "Invalid input, please try again"
